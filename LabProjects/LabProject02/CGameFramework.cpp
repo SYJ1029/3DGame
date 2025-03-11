9f66ee608642df358264f03cd5a830f9f761da52
SYJ1029 <abg0986@gmail.com>
2025-03-11 22:32:20 +0900

    새 프로젝트 수행 과정에서 예기치 못한 컴파일 에러가 나타나고 있다.

---
+++ LabProjects/LabProject02/CGameFramework.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "CGameFramework.h"
+
+
+CGameFramework::CGameFramework() 
+{
+	m_pdxgiFactory = NULL;
+	m_pdxgiSwapChain = NULL;
+	m_pd3dDevice = NULL;
+	m_pd3dCommandAllocator = NULL;
+	m_pd3dCommandQueue = NULL;
+	m_pd3dPipelineState = NULL;
+	m_pd3dCommandList = NULL;
+	for (int i = 0; i < m_nSwapChainBuffers; i++) 
+		m_ppd3dRenderTargetBuffers[i] = NULL;
+	m_pd3dRtvDescriptorHeap = NULL;
+	m_nRtvDescriptorIncrementSize = 0;
+	m_pd3dDepthStencilBuffer = NULL;
+	m_pd3dDsvDescriptorHeap = NULL;
+	m_nDsvDescriptorIncrementSize = 0;
+	m_nSwapChainBufferIndex = 0;
+	m_hFenceEvent = NULL;
+	m_pd3dFence = NULL;
+	m_nFenceValue = 0;
+	m_nWndClientWidth = FRAME_BUFFER_WIDTH;
+	m_nWndClientHeight = FRAME_BUFFER_HEIGHT;
+}
+
+CGameFramework::~CGameFramework()
+{
+
+}
+
+//다음 함수는 응용 프로그램이 실행되어 주 윈도우가 생성되면 호출된다는 것에 유의하라. 
+bool CGameFramework::OnCreate(HINSTANCE hInstance, HWND hMainWnd)
+{
+	m_hInstance = hInstance;
+	m_hWnd = hMainWnd;
+
+	//Direct3D 디바이스, 명령 큐와 명령 리스트, 스왑 체인 등을 생성하는 함수를 호출한다. 
+	CreateDirect3DDevice();
+	CreateCommandQueueAndList();
+	CreateSwapChain();
+	CreateRtvAndDsvDescriptorHeaps();
+	CreateRenderTargetViews();
+	CreateDepthStencilView();
+	BuildObjects();
+	//렌더링할 게임 객체를 생성한다. 
+	return(true);
+}
