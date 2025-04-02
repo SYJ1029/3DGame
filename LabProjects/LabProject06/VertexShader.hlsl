7ab6b80c71dcdfab33dd30d1f0837c56e28cb88e
SYJ1029 <abg0986@gmail.com>
2025-04-03 00:03:53 +0900

    따라하기 07준비

---
+++ LabProjects/LabProject06/VertexShader.hlsl
@@ -0,0 +1,23 @@
+float4 VSMain( uint nVertexID : SV_VertexID) : SV_Position
+{
+    float4 output = (float4)0;
+    
+     //프리미티브(삼각형)를 구성하는 정점의 인덱스(SV_VertexID)에 따라 정점을 반환한다.
+    //정점의 위치 좌표는 변환이 된 좌표(SV_POSITION)이다. 즉, 투영좌표계의 좌표이다.
+    if (nVertexID == 0)
+        output = float4(-1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 1)
+        output = float4(+1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 2)
+        output = float4(+1.0f, -1.0f, 1.0f, 1.0f);
+    else if (nVertexID == 3)
+        output = float4(-1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 4)
+        output = float4(+1.0f, -1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 5)
+        output = float4(-1.0f, -1.0f, 0.0f, 1.0f);
+    // 사각형의 각 정점에 맞춰서 좌표를 설정
+    
+    return (output);
+}
+
