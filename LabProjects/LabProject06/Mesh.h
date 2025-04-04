fc1950a2a3f686c0395e99028e7eb7086ac5ba7a
LAPTOP-KAKG2S7N\abg09 <abg0986@gmail.com>
2025-04-04 15:06:35 +0900

    1. 파일 추가 2. vcxproj수정, winmm.lib를 가져오는 부분을 stdafx선에서 수행할 수 있도록

---
+++ LabProjects/LabProject06/Mesh.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class CVertex
+{
+protected:
+
+};
+
+class CMesh
+{
+};
+
--- LabProjects/LabProject06/stdafx.h
+++ LabProjects/LabProject06/stdafx.h
@@ -39,6 +39,8 @@ using Microsoft::WRL::ComPtr;
 
 #pragma comment(lib, "dxguid.lib")
 
+#pragma comment(lib, "winmm.lib")
+
 #define FRAME_BUFFER_WIDTH 800
 #define FRAME_BUFFER_HEIGHT 600
 
